06c873d07ee990e1224c8b0d2d0339a61d3e276c
Skitt2 <provaid@yandex.ru>
2022-03-28 22:40:51 +0300

    Добавьте файлы проекта.

---
+++ DZ11/LinUrovnenie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ11
+{
+    struct LinUrovnenie
+    {
+        double k;
+        double b;
+
+        public LinUrovnenie(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0 && b != 0)
+                return "Решений нет";
+            if (k == 0 || b == 0)
+                return "Является любое число";
+            double x = k / b;
+            return $"Решение уравнения = {x}";
+        }
+
+    }
+}
---
+++ DZ11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Разработать структуру для решения линейного уравнения 0=kx+b. Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. Осуществить использование экземпляра в программе.");
+            Console.Write("k= ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b= ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            LinUrovnenie linUrovnenie = new LinUrovnenie(k, b);
+            Console.WriteLine(linUrovnenie.Root());
+            Console.ReadKey();
+        }
+    }
+}
